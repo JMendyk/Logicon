f123d2f17581a9b94cd89328e732e86dee0bc0df
Jakub Mendyk <jakubmendyk.szkola+git@gmail.com>
2017-12-22 18:51:33 +0100

    Implement dummy MenuWidget.
    Implements MenuWidget's layout as specified in documentation.
    However all displayed data is static and does not reflect real
    data/status of the app.
    
    Particularly, open/save buttons require adding solution for displaying
    file picker dialog.

--- src/widgets/menu_widget.cpp
+++ src/widgets/menu_widget.cpp
@@ -24,7 +24,48 @@ namespace Logicon {
     }
 
     void MenuWidget::render_ui(const ImVec2 &window_pos, const ImVec2 &window_size) {
+        ImGuiWindowFlags window_flags =
+                0
+                | ImGuiWindowFlags_NoTitleBar
+                | ImGuiWindowFlags_NoResize
+                | ImGuiWindowFlags_NoMove;
 
+        ImGui::Begin("Actions Window", nullptr, window_flags);
+        {
+            ImGui::SetWindowPos(window_pos, ImGuiCond_Always);
+            ImGui::SetWindowSize(window_size, ImGuiCond_Always);
+
+            const int sz = static_cast<int>(window_size.y - 16);
+
+            ImGui::Button("New", ImVec2(sz, sz));
+            ImGui::SameLine();
+            ImGui::Button("Open", ImVec2(sz, sz));
+            ImGui::SameLine();
+            ImGui::Button("Save", ImVec2(sz, sz));
+            ImGui::SameLine();
+
+            ImGui::Button("Play", ImVec2(sz, sz));
+            ImGui::SameLine();
+            ImGui::Button("Stop", ImVec2(sz, sz));
+            ImGui::SameLine();
+            ImGui::Button("Reset", ImVec2(sz, sz));
+            ImGui::SameLine();
+
+            static int tickrate = 1000;
+
+            ImGui::PushItemWidth(256);
+            ImGui::PushFont(myFont);
+            ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(8, 8));
+            ImGui::InputInt("", &tickrate, NULL, NULL);
+            ImGui::PopStyleVar();
+            ImGui::PopFont();
+            ImGui::PopItemWidth();
+
+            ImGui::SameLine();
+            ImGui::Button("Step-by-step", ImVec2(sz, sz));
+            ImGui::SameLine();
+        }
+        ImGui::End();
     }
 
-};
+} // namespace Logicon

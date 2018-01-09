ca2e92c5bd0fd571dadaff14605f1848b807ce02
kralizek <lukwroblewski999@gmail.com>
2018-01-09 18:51:14 +0100

    Add switch gate

--- include/gates/switch.h
+++ include/gates/switch.h
@@ -1,5 +1,5 @@
 //
-// Created by rufus on 11.12.17.
+// Created by kralizekmd on 09.01.2018
 //
 
 #ifndef LOGICON_SWITCH_H
@@ -15,6 +15,10 @@ namespace Logicon{
      * if it isn't, then the output is LOW
      */
     class Switch : public Gate {
+
+    private:
+        /// switch state
+        int flag;
 
     public:
         /// Constructor: default state is LOW
--- src/gates/switch.cpp
+++ src/gates/switch.cpp
@@ -1,17 +1,19 @@
 //
-// Created by rufus on 11.12.17.
+// Created by kralizekmd on 09.01.2018
 //
 
 #include "gates/switch.h"
 
 namespace Logicon {
-    Switch::Switch(ID id) : Gate(GATE_TYPE::SWITCH, id) {}
+    Switch::Switch(ID id) : Gate(GATE_TYPE::SWITCH, id) {
+        this->flag=0;
+    }
 
     void Switch::update() {
-
+        this->setOutputState(0,this->getInputState(0));
     }
 
     void Switch::clickAction() {
-        Gate::clickAction();
+        this->flag^=1;
     }
 } // namespace Logicon

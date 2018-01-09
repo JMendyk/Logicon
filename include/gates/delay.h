41425de1ccfcbc0e70a8c06480b5e665fbc11191
kralizek <lukwroblewski999@gmail.com>
2018-01-09 18:40:23 +0100

    Add delay gate

--- include/gates/delay.h
+++ include/gates/delay.h
@@ -1,5 +1,5 @@
 //
-// Created by rufus on 12.12.17.
+// Created by kralizekmd on 09.01.2018
 //
 
 #ifndef LOGICON_DELAY_H
@@ -25,9 +25,11 @@ namespace Logicon {
     private:
         /// how much the input signal is shifted into the future. can't be less than 0.
         Tick delay;
+        /// list of waiting signals
+        std::list<int> signals;
 
     public:
-        /// Constructor constructs default 1 tick delay
+        /// Constructor constructs default 1 tick delay and list contains one element equal 0
         explicit Delay(ID id);
 
         /**
@@ -37,6 +39,7 @@ namespace Logicon {
 
         /**
          * Changes settings for delay.
+         * if new delay is smaller the oldest signals are lost
          * @param delay new value for delay in Ticks
          */
         void changeSettings(Tick delay);
--- src/gates/delay.cpp
+++ src/gates/delay.cpp
@@ -1,18 +1,26 @@
 //
-// Created by rufus on 12.12.17.
+// Created by kralizekmd on 09.01.2018
 //
 
 #include "gates/delay.h"
 
 namespace Logicon {
-    Delay::Delay(ID id) : Gate(GATE_TYPE::DELAY, id) {}
-
+    Delay::Delay(ID id) : Gate(GATE_TYPE::DELAY, id) {
+        this->delay=1;
+        this->signals.push_back(0);
+    }
 
     void Delay::update() {
-
+        if(this->delay==0) this->setOutputState(0,this->getInputState(0));
+        else{
+            this->setOutputState(0,this->signals.back());
+            this->signals.pop_back();
+            this->signals.push_front(this->getInputState(0));
+        }
     }
 
     void Delay::changeSettings(Tick delay) {
-
+        this->delay=delay;
+        this->signals.resize(delay,0);
     }
 } // namespace Logicon

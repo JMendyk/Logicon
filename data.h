5a15ecb1c38696b9d19e66658393058b6fbab7e0
walkowiakm <33702176+walkowiakm@users.noreply.github.com>
2018-01-10 20:07:58 +0100

    to_json for Delay

--- data.h
+++ data.h
@@ -15,21 +15,28 @@ namespace Logicon {
     class Data {
         
         void from_json(const json& j, Gate& g){
+        	//Gate
         	g.id = j.at("id").get<ID>();
         	g.gateType = j.at("type").get<GATE_TYPE>();
         	int n = g.getInputsCount();
         	for(int i=0;i<n;i++) g.setInputState(j["inputs"][i]["state"]);
         	int m = g.getOutputsCount();
         	for(int i=0;i<m;i++) g.setOutputState(j["outputs"][i]["state"]);
+        	//Delay
+        	
 		}
 		
         void to_json(const Gate& g, json& j){
+        	//Gate
         	j ["id"] = g.id
 			j ["type"] = g.gateType;
         	int n = g.getInputsCount();
         	for(int i=0;i<n;i++) j["inputs"][i]["state"] = g.getInputState(i);
         	int m = g.getOutputsCount();
         	for(int i=0;i<m;i++) j["outputs"][i]["state"] = g.getOutputState(i);
+        	//Delay
+        	j ["delay"] = g.getDelay();
+        	j ["signals"] = g.getDelayList();
 		}
         
     };

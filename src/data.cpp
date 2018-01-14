d2b990e285626e54b77d046a5af15f2cb7783cab
Rufusmen <rafusmen@gmail.com>
2018-01-14 05:58:45 +0100

    Data class fully implemented
    Testing of saving and loading needed

--- src/data.cpp
+++ src/data.cpp
@@ -21,14 +21,37 @@ namespace Logicon{
     void Data::save(std::string path, std::shared_ptr<GCircuit> gCircuit) {
         const std::shared_ptr<Circuit> circuit = gCircuit->getCircuit();
         nlohmann::json j_gates = nlohmann::json::array();
-        for (auto Gate : circuit->getGates()) {
-            auto pos = gCircuit->getGBlockByID(Gate->id)->getPosition();
-            nlohmann::json j_gate = {{"id",      Gate->id},
-                                     {"x",       pos.x},
-                                     {"y",       pos.y},
-                                     {"type",    Gate->getGateType()},
-                                     {"in_cnt",  Gate->getInputsCount()},
-                                     {"out_cnt", Gate->getOutputsCount()}};
+        for (auto gate : circuit->getGates()) {
+            auto pos = gCircuit->getGBlockByID(gate->id)->getPosition();
+            nlohmann::json j_in_ports = nlohmann::json::array();
+            auto in_ports = gate->getInputs();
+            for (int i = 0; i < in_ports.size(); i++) {
+                nlohmann::json j_in_port = nlohmann::json::array();
+                auto in_port = in_ports[i].second;
+                for (int l = 0; l < in_port.size(); l++) {
+                    j_in_port += {{"id",   in_port[l].id},
+                                  {"port", in_port[l].port}};
+                }
+                nlohmann::json j_in = {{"state", in_ports[i].first},
+                                       {"in",    j_in_port}};
+                j_in_ports += j_in;
+            }
+            auto out = gate->getOutputs()[0];
+            nlohmann::json j_out_conn = nlohmann::json::array();
+            for (int i = 0; i < out.second.size(); i++) {
+                nlohmann::json conn = {{"id",   out.second[i].id},
+                                       {"port", out.second[i].port}};
+                j_out_conn += conn;
+            }
+            nlohmann::json j_out = {{"state", out.first},
+                                    {"ports", j_out_conn}};
+
+            nlohmann::json j_gate = {{"id",   gate->id},
+                                     {"x",    pos.x},
+                                     {"y",    pos.y},
+                                     {"type", gate->getGateType()},
+                                     {"in",   j_in_ports},
+                                     {"out",  j_out}};
             j_gates += j_gate;
         }
         nlohmann::json j_circuit = {{"flag", circuit->INITIALIZED_FLAG},
@@ -49,13 +72,13 @@ namespace Logicon{
         std::shared_ptr<Circuit> circuit = std::make_shared<Circuit>(j.value("id", 0));
         std::shared_ptr<GCircuit> gCircuit = std::make_shared<GCircuit>(circuit);
         circuit->INITIALIZED_FLAG = j.value("flag", 0);
-        nlohmann::json arr = j["list"];
-        std::cout << arr << std::endl;
-        std::cout.flush();
+        auto arr = j["list"];
+        //std::cout << arr << std::endl;
+        //std::cout.flush();
         for (int i = 0; i < arr.size(); i++) {
             nlohmann::json j_gate = arr[i];
-            std::cout << j_gate;
-            std::cout.flush();
+            //std::cout << j_gate;
+            //std::cout.flush();
             std::shared_ptr<Gate> gate;
             GATE_TYPE type = (GATE_TYPE) (j_gate.value("type", -1));
             ID id = j_gate.value("id", 1);
@@ -104,6 +127,24 @@ namespace Logicon{
                     gate = std::make_shared<Input>(id);
                     std::static_pointer_cast<Input, Gate>(gate)->setClicked(false);
                     break;
+            }
+            auto ins = j_gate["in"];
+            auto out = j_gate["out"];
+            std::cout << ins << std::endl;
+            gate->setOutputState(0, out["state"]);
+            auto out_conns = out["ports"];
+            for (int l = 0; l < out_conns.size(); l++) {
+                gate->addOutputConnection(0, out_conns[l]["id"], out_conns[l]["port"]);
+            }
+            for (int l = 0; l < ins.size(); l++) {
+                auto in = ins[l];
+                auto in_port = in["in"];
+                std::cout << in << std::endl;
+                std::cout << in_port << std::endl;
+                for (int r = 0; r < in_port.size(); r++) {
+                    gate->setInputConnection(l, in_port[r].value("id", 0), in_port[r].value("port", 0));
+                }
+                gate->setInputState(l, in.value("state", 0));
             }
             circuit->add(gate);
             gCircuit->insert(gate, pos);
